c4dc4f8e34246f2f2592da9e168357c94aa0d75c
Freddie Vargus <fjv41995@gmail.com>
2025-09-01 12:05:50 -0400

    Remove formatter

--- mealworm/agents/__init__.py
+++ mealworm/agents/__init__.py
@@ -1,11 +1,9 @@
 from .fetcher import MealFetcherAgent
 from .analyzer import MealAnalyzerAgent
 from .generator import MealPlanGeneratorAgent
-from .formatter import MealPlanFormatterAgent
 
 __all__ = [
     "MealFetcherAgent",
     "MealAnalyzerAgent", 
     "MealPlanGeneratorAgent",
-    "MealPlanFormatterAgent"
 ]
