2d597aa6ed3a69c2215d596cac828d203a34c31c
Freddie Vargus <fjv41995@gmail.com>
2025-09-01 16:13:48 -0400

    Update test_workflow.py

--- evals/test_workflow.py
+++ evals/test_workflow.py
@@ -1,3 +1,6 @@
 # make sure the final meal plan is a a valid format
 # make sure the <start-of-format> and <end-of-format> tags are NOT present
 # make sure the meal plan is valid markdown
+# ensure there is only one "eat out" day
+# ensure there are leftovers for lunch for 5 days
+# ensure there is 2 chicken dishes, 1 fish dish, and vegetables in every dish
