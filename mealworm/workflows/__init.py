abe16d599b12c4ffb0e1bd9d10e6f83547602381
Freddie Vargus <fjv41995@gmail.com>
2025-09-25 16:09:10 -0400

    Delete mealworm/workflows directory

--- mealworm/workflows/__init.py
+++
@@ -1,9 +0,0 @@
-from .fetcher import MealFetcherAgent
-from .analyzer import MealAnalyzerAgent
-from .generator import MealPlanGeneratorAgent
-
-__all__ = [
-    "MealFetcherAgent",
-    "MealAnalyzerAgent", 
-    "MealPlanGeneratorAgent",
-]
